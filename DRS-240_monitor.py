00e5d6fbcfb6daa2725c3976566a9b196784d36a
hwanetick <hwanetick@gmail.com>
2025-06-06 09:39:08 -0700

    Monitor v1.0 - Startup v1.0 - Generic for Windows
    Release of Meanwell DRS UPS scripts for Windows computers

--- DRS-240_monitor.py
+++ DRS-240_monitor.py
@@ -1,32 +1,23 @@
 #!/usr/bin/env python3
 #
 # Douglas Alden - 13 Mar 2024
-# Edited for YLI - 8 Aug 2024 - hkw
+# Generic version for Windows logger systems - HKW - 18 Apr 2025
+# Branch: RADMet_UPS - Version: 1.0
 
 import minimalmodbus
 from datetime import datetime
 import string
 import os
+import sys
+import logging
+from logging.handlers import RotatingFileHandler
 
 # Modbus RTU device parameters
-port = "COM1" #'/dev/ttyUSB0'
-baudrate = 115200
-databits = 8
-parity = 'N'
-stopbits = 1
-address = 0x83  # Modbus device address
+default_port = "COM1"
+default_address = 0x83  # Modbus device address
 
 # Create a minimalmodbus instrument
-instrument = minimalmodbus.Instrument(port, address)
-instrument.serial.baudrate = baudrate
-instrument.serial.bytesize = databits
-instrument.serial.parity = parity
-instrument.serial.stopbits = stopbits
-
-#print(instrument.serial)
-#print("")
-#print("Modbus address:",hex(instrument.address))
-#print("")
+instrument = minimalmodbus.Instrument(default_port, default_address)
 
 # Function Codes
 READ_HOLDING_REGISTER = 0x03
@@ -38,6 +29,7 @@ FAULT_STATUS = 0x40
 READ_VIN = 0x50
 READ_VOUT = 0x60
 READ_IOUT = 0x61
+READ_TEMP = 0x62
 CHG_STATUS = 0xB8
 SYSTEM_STATUS = 0xC3
 UPS_CONFIG = 0xD2
@@ -46,7 +38,27 @@ TIME_BUFFERING = 0xE4
 UPS_DELAY_TIME = 0xE8
 UPS_RESTART_TIME = 0xE9
 
-def read_registers(instrument, register_address, number_of_bytes, functioncode):
+STATUS_LOG_FILE = "D:\\modbus\\Meanwell_UPS_status.log"
+EVENTS_LOG_FILE = "D:\\modbus\\Meanwell_UPS_events.log"
+MAX_LOG_SIZE = 6613800  #2 months status logging per file w/5 backup files = 1 year logging
+
+telemetry_file_path = "D:\\modbus\\Meanwell_UPS_Telemetry_Data.csv"
+
+formatter1 = logging.Formatter("%(asctime)s - %(levelname)s: %(message)s","%Y-%m-%d %H:%M:%S")
+handler1 = RotatingFileHandler(EVENTS_LOG_FILE, maxBytes=MAX_LOG_SIZE, backupCount=1, encoding='utf-8')
+handler1.setFormatter(formatter1)
+events_logger = logging.getLogger("RotatingLogger1")
+events_logger.setLevel(logging.INFO)
+events_logger.addHandler(handler1)
+
+formatter2 = logging.Formatter("%(asctime)s - %(message)s","%Y-%m-%d %H:%M:%S")
+handler2 = RotatingFileHandler(STATUS_LOG_FILE, maxBytes=MAX_LOG_SIZE, backupCount=5, encoding='utf-8')
+handler2.setFormatter(formatter2)
+status_logger = logging.getLogger("RotatingLogger2")
+status_logger.addHandler(handler2)
+
+def read_registers(register_address, number_of_bytes, functioncode):
+    global instrument
     try:
         # Read bytes from the specified input register
         values = instrument.read_registers(register_address, number_of_bytes, functioncode)
@@ -54,11 +66,13 @@ def read_registers(instrument, register_address, number_of_bytes, functioncode):
         if (register_address == FAULT_STATUS):
             value = values[0]
         elif (register_address == READ_VIN):
-            value = values[0]/100
+            value = values[0]/10
         elif (register_address == READ_VOUT):
             value = values[0]/100
         elif (register_address == READ_IOUT):
             value = values[0]/100
+        elif (register_address == READ_TEMP):
+            value = values[0]/10
         elif (register_address == CHG_STATUS):
             value = values[0]
         elif (register_address == SYSTEM_STATUS):
@@ -77,147 +91,137 @@ def read_registers(instrument, register_address, number_of_bytes, functioncode):
         return value
 
     except Exception as e:
-        print(f"{e}")
+        events_logger.exception(f"{e}")
         return -99999 
 
-#def write_to_register(instrument, register_address, value):
-#    try:
-#        # Write one byte to the specified register
-#        instrument.write_register(register_address, value, functioncode=6)
-#
-#    except Exception as e:
-#        print(f"An error occurred during writing: {e}")
-
-def save_to_file(values, file_path):
+def save_to_file(values, telemetry_file_path):
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    line = timestamp + ',' + values + '\n'
+    line = timestamp + ',' + values
 
-    with open(file_path, 'a') as file:
+    with open(telemetry_file_path, 'w') as file:
         file.write(line)
         file.close()
 
 def shutdown_IPC(): # Shutdown the computer
-    ups_shutdown = 'shutdown -s -t 60 -c "UPS System Shutdown"'  #'sudo shutdown -h +1 "UPS System Shutdown"'
-    file_path =  "D:\\modbus\\DRS-240_events.log"     #'/Data/modbus/DRS-240_events.log'   
+    ups_shutdown = 'shutdown -s -t 60 -c "UPS System Shutdown"' 
     Status_out = "Shutdown,UPS System Shutdown"
-    save_to_file(Status_out, file_path)
-
+    events_logger.warning(Status_out)
     os.system(ups_shutdown)
-    
-def main():
 
-#    # Power supply parameters
-#
-#    # UPS_CONFIG
-#    # default: 0x09
-#    ups_config = 0x09
-#    # Turn off Wake_Up_EN
-#    ups_config = ups_config & 0b00000000
-#    # Enable time buffering
-#    ups_config = ups_config | 0b00000100
-#    # Set UPS_Delay_EN
-#    ups_config = ups_config | 0b00010000
-#    # Set UPS_Shutdown_EN
-#    ups_config = ups_config | 0b00100000
-#    # new value: 0x3D
-#    write_to_register(instrument, UPS_CONFIG, ups_config)
-#    # read ups_config
-#    ups_config  = read_registers(instrument, UPS_CONFIG, 1, READ_HOLDING_REGISTER)
-#    bin_ups_config = bin(ups_config)
-#    print("0x%02X - UPS_Config: 0x%02X %s" % (UPS_CONFIG, ups_config, bin_ups_config))
-    
-#    # TIME_BUFFERING
-#    # default: 600 minutes
-#    # Set to 600 minutes
-#    write_to_register(instrument, TIME_BUFFERING, 60)
-#    # read TIME_BUFFERING
-#    time_buffer = read_registers(instrument, TIME_BUFFERING, 1, READ_HOLDING_REGISTER)
-#    print("0x%02X - Time Buffer: %d min" % (TIME_BUFFERING, time_buffer))
+def main() :
+    global instrument
+    try :
 
-#    # Set UPS delay time to 180 seconds
-#    #write_to_register(instrument, UPS_DELAY_TIME, 180)
-#    # Read UPS delay time
-#    Delay_time = read_registers(instrument, UPS_DELAY_TIME, 1, READ_HOLDING_REGISTER)
-#    print("0x%02X - Delay Time: %d sec" % (UPS_DELAY_TIME, Delay_time))
-    
-#    # Read UPS restart time
-#    Restart_Time = read_registers(instrument, UPS_RESTART_TIME, 1, READ_HOLDING_REGISTER)
-#    print("0x%02X - Restart Time: %d sec" % (UPS_RESTART_TIME, Restart_Time))
+        # Read port number from command line arguments
+        if len(sys.argv) != 2:
+            sys.exit(0)
 
-    # Power Supply Status
-    V_out = read_registers(instrument, READ_VOUT, 2, READ_INPUT_REGISTER)
-    I_out = read_registers(instrument, READ_IOUT, 2, READ_INPUT_REGISTER)
-    V_Bat = read_registers(instrument, READ_VBAT, 2, READ_INPUT_REGISTER)
-    
-    # Check if AC is still online
-    ac_status = read_registers(instrument, FAULT_STATUS, 1, READ_HOLDING_REGISTER)
-    if (ac_status != -99999): # Value was read
-        ac_status = (ac_status >> 5) & 0x01
-        if (ac_status == 0):
-            ac_status = "AC OK"
+        # Meanwell DRS-240-12 or DRS-480-24 DC UPS is connected to the rugged computer using an Advantech Isolated RS-422/485 to RS-232 Converter BB-4WSD9OTB.
+ 
+        comPort = sys.argv[1]
+
+        if comPort != default_port:
+            instrument = minimalmodbus.Instrument(comPort, default_address)
+
+        instrument.serial.baudrate = 115200
+        instrument.serial.bytesize = 8
+        instrument.serial.parity = 'N'
+        instrument.serial.stopbits = 1
+
+        status_byte = 0b00000000
+
+        # Power Supply Status
+        V_out = read_registers(READ_VOUT, 2, READ_INPUT_REGISTER)
+        I_out = read_registers(READ_IOUT, 2, READ_INPUT_REGISTER)
+        V_Bat = read_registers(READ_VBAT, 2, READ_INPUT_REGISTER)
+        V_in = read_registers(READ_VIN, 1, READ_INPUT_REGISTER)
+        T_amb = read_registers(READ_TEMP, 1, READ_INPUT_REGISTER)
+
+        # Check if AC is still online
+        ac_status = read_registers(FAULT_STATUS, 1, READ_HOLDING_REGISTER)
+        if (ac_status != -99999): # Value was read
+            ac_status = (ac_status >> 5) & 0x01
+            status_byte = status_byte | ac_status
+            if (ac_status == 0):
+                AC_status = "AC OK"
+            else:
+                AC_status = "AC DOWN"
         else:
-            ac_status = "AC DOWN"
-    
-    # Check if timeout buffer has been reached.
-    chg_status = read_registers(instrument, CHG_STATUS, 1, READ_HOLDING_REGISTER)
-    if (chg_status != -99999):  # Value was read
-        message = "0x%02X - Charge Status: " % (CHG_STATUS)
-        battery_detect = (chg_status >> 11) & 0x01
-        if (battery_detect == 0):
-            message = message + "Battery Detected"
-        else :
-            message = message + "No Battery"
-        buffer_timeout = (chg_status >> 12) & 0x01
-        if (buffer_timeout == 0) :
-            message = message + ", No Timeout"
-        else :
-            message = message + ", Timeout Reached"
+            AC_status = "NAN"  #communication failure
 
-        #print(message)
-    
-    # Check if DC output is
-    system_status = read_registers(instrument, SYSTEM_STATUS, 1, READ_HOLDING_REGISTER)
-    if (system_status != -99999):  # Value was read
-        dc_ok = (system_status & 0b00000010) >> 1
-        chg_ups_status = (system_status & 0b10000000) >> 7
+        # Check if timeout buffer has been reached.
+        chg_status = read_registers(CHG_STATUS, 1, READ_HOLDING_REGISTER)
+        if (chg_status != -99999):  # Value was read
+            battery_detect = (chg_status >> 11) & 0x01
+            status_byte = status_byte | ((battery_detect << 1)& 0x02)
+            if (battery_detect == 0):
+                Battery_Detect = "Battery"
+            else :
+                Battery_Detect = "No Batt"
+            buffer_timeout = (chg_status >> 12) & 0x01
+            status_byte = status_byte | ((buffer_timeout << 2)& 0x04)
+            if (buffer_timeout == 0) :
+                Timeout = "No Timeout"
+            else :
+                Timeout = "Timeout Reached"
+        else:
+            Battery_Detect = "NAN"  #communication failure
+            Timeout =  "NAN"
+            buffer_timeout = 0
+
+
+        # Check if DC output is
+        system_status = read_registers(SYSTEM_STATUS, 1, READ_HOLDING_REGISTER)
+        if (system_status != -99999):  # Value was read
+            dc_ok = (system_status & 0b00000010) >> 1
+            chg_ups_status = (system_status & 0b10000000) >> 7
+            status_byte = status_byte | ((dc_ok << 3)& 0x08) | ((chg_ups_status << 4)& 0x10)
+            if (chg_ups_status == 1):
+                Chg_UPS_Status = "UPS Mode"
+            else:
+                Chg_UPS_Status = "Charge Mode"            
             
-    #print("")
-    #print(f"System stats")
-    #print(f"      V Out:", V_out, "V")
-    #print(f"      I Out:", I_out, "A")
-    #print(f"     Batt V:", V_Bat, "V")
-    #print(f"   AC Input:", ac_status)
-    
-    # if (chg_ups_status == 1):
-        # print(f"Operating Mode: UPS")
-    # else:
-        # print(f"Operating Mode: Charging")
-    
-    # if (dc_ok == 1):
-        # print(f"Secondary DD output voltage status: NORMAL")
-    # else:
-        # print(f"Secondary DD output voltage status: TOO LOW")
+            if (dc_ok == 1):
+                DC_OK = "DC V_out good"
+            else:
+                DC_OK = "DC V_out low"
 
-    Status_out = "%.2f,%.2f,%.2f,%s,%d,%d,%d" % (V_out, I_out, V_Bat, ac_status, chg_ups_status, dc_ok, buffer_timeout)
-#    Status_out = "%.2f,%.2f,%.2f,%s,%d" % (V_out, I_out, V_Bat, ac_status, dc_ok)
+        else:
+            Chg_UPS_Status =  "NAN"  #communication failure
+            DC_OK =  "NAN"
+            dc_ok = 1
 
-    file_path = "D:\\modbus\\DRS-240_status.txt"  #'/Data/modbus/DRS-240_status.txt'
-    save_to_file(Status_out, file_path)    
-    # Close the serial connection
-    instrument.serial.close()
-    
-    # If the buffer_timeout has been reached or the low battery alarm is set shutdown the computer
-    if (buffer_timeout | (dc_ok == 0)):
-        file_path = "D:\\modbus\\DRS-240_events.log"  #'/Data/modbus/DRS-240_events.log'
-        if (buffer_timeout):
-            Status_out = "Shutdown,buffer_timeout"
-            save_to_file(Status_out, file_path)
-        if (dc_ok == 0):
-            Status_out = "Shutdown,low_battery"
-            save_to_file(Status_out, file_path)
 
-        shutdown_IPC()
-    
+        Status_out = "Vout = %.2f V, Iout = %.2f A, Vbat = %.2f V, Vin = %.1f V, Temp = %.1f C, %s, %s, %s, %s, %s" % (V_out, I_out, V_Bat, V_in, T_amb, AC_status, Battery_Detect, Chg_UPS_Status, DC_OK, Timeout)
+        status_logger.warning(Status_out)
+
+        telemetry_status_out = "%.2f,%.2f,%.2f,%.1f,%.1f,%02X" % (V_out, I_out, V_Bat, V_in, T_amb, status_byte)
+
+        save_to_file(telemetry_status_out, telemetry_file_path)
+
+        # Close the serial connection
+        instrument.serial.close()
+
+        # If the buffer_timeout has been reached or the low battery alarm is set shutdown the computer
+        if (buffer_timeout | (dc_ok == 0)):
+            #file_path = "D:\\modbus\\DRS-240_events.log"
+            if (buffer_timeout):
+                Status_out = "Shutdown,buffer_timeout"
+                events_logger.warning(Status_out)
+
+            if (dc_ok == 0):
+                Status_out = "Shutdown,low_battery"
+                events_logger.warning(Status_out)
+
+
+            shutdown_IPC()
+
+
+    except KeyboardInterrupt :
+        events_logger.exception("keyboard_interrupt")
+    except Exception as e:
+        events_logger.exception(f"{e}")
+    sys.exit(0)
 
 if __name__ == "__main__":
     main()
--- DRS-240_startup.py
+++ DRS-240_startup.py
@@ -1,30 +1,25 @@
 #!/usr/bin/env python3
+#
+# Douglas Alden - 13 Mar 2024
+# Generic version for Windows logger systems - HKW - 18 Apr 2025
+# Branch: RADMet_UPS - Version: 1.0
 
 import minimalmodbus
 from datetime import datetime
 import string
 import time
 import os
+import sys
+import logging
+from logging.handlers import RotatingFileHandler
 
 # Modbus RTU device parameters
-port = "COM1"
-baudrate = 115200
-databits = 8
-parity = 'N'
-stopbits = 1
-address = 0x83  # Modbus device address
+default_port = "COM1"
+default_address = 0x83  # Modbus device address
+default_timer = 600
 
 # Create a minimalmodbus instrument
-instrument = minimalmodbus.Instrument(port, address)
-instrument.serial.baudrate = baudrate
-instrument.serial.bytesize = databits
-instrument.serial.parity = parity
-instrument.serial.stopbits = stopbits
-
-print(instrument.serial)
-print("")
-print("Modbus address:",hex(instrument.address))
-print("")
+instrument = minimalmodbus.Instrument(default_port, default_address)
 
 # Function Codes
 READ_HOLDING_REGISTER = 0x03
@@ -36,6 +31,7 @@ FAULT_STATUS = 0x40
 READ_VIN = 0x50
 READ_VOUT = 0x60
 READ_IOUT = 0x61
+READ_TEMP = 0x62
 CHG_STATUS = 0xB8
 SYSTEM_STATUS = 0xC3
 UPS_CONFIG = 0xD2
@@ -44,7 +40,20 @@ TIME_BUFFERING = 0xE4
 UPS_DELAY_TIME = 0xE8
 UPS_RESTART_TIME = 0xE9
 
-def read_registers(instrument, register_address, number_of_bytes, functioncode):
+EVENTS_LOG_FILE = "D:\\modbus\\Meanwell_UPS_events.log"
+MAX_LOG_SIZE = 6613800
+
+formatter1 = logging.Formatter("%(asctime)s - %(levelname)s: %(message)s","%Y-%m-%d %H:%M:%S") 
+
+handler1 = RotatingFileHandler(EVENTS_LOG_FILE, maxBytes=MAX_LOG_SIZE, backupCount=1, encoding='utf-8')
+handler1.setFormatter(formatter1)
+events_logger = logging.getLogger("RotatingLogger1")
+events_logger.setLevel(logging.INFO)
+events_logger.addHandler(handler1)
+
+
+def read_registers(register_address, number_of_bytes, functioncode):
+    global instrument
     try:
         # Read bytes from the specified input register
         values = instrument.read_registers(register_address, number_of_bytes, functioncode)
@@ -52,11 +61,13 @@ def read_registers(instrument, register_address, number_of_bytes, functioncode):
         if (register_address == FAULT_STATUS):
             value = values[0]
         elif (register_address == READ_VIN):
-            value = values[0]/100
+            value = values[0]/10
         elif (register_address == READ_VOUT):
             value = values[0]/100
         elif (register_address == READ_IOUT):
             value = values[0]/100
+        elif (register_address == READ_TEMP):
+            value = values[0]/10
         elif (register_address == CHG_STATUS):
             value = values[0]
         elif (register_address == SYSTEM_STATUS):
@@ -75,68 +86,95 @@ def read_registers(instrument, register_address, number_of_bytes, functioncode):
         return value
 
     except Exception as e:
-        print(f"{e}")
+        events_logger.exception(f"{e}")
         return -99999 
 
 
-def write_to_register(instrument, register_address, value):
+def write_to_register(register_address, value):
+    global instrument
     try:
         # Write one byte to the specified register
         instrument.write_register(register_address, value, functioncode=6)
 
     except Exception as e:
-        print(f"An error occurred during writing: {e}")
+        events_logger.exception(f"An error occurred during writing: {e}")
 
-def save_to_file(values, file_path):
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    line = timestamp + ',' + values + '\n'
 
-    with open(file_path, 'a') as file:
-        file.write(line)
-        file.close()
+def main() :
+    global instrument
+    try :
 
-def main():
+        # Read port number from command line arguments
+        if len(sys.argv) != 3:
+            sys.exit(0)
 
-    # UPS_CONFIG
-    # default: 0x09
-    ups_config = 0x09
-    # Turn off Wake_Up_EN
-    ups_config = ups_config & 0b00000000
-    # Enable time buffering
-    ups_config = ups_config | 0b00000100
-    # Set UPS_Delay_EN
-    ups_config = ups_config | 0b00010000
-    # Set UPS_Shutdown_EN
-    ups_config = ups_config | 0b00100000
-    # new value: 0x34
-    write_to_register(instrument, UPS_CONFIG, ups_config)
-    # read ups_config
-    ups_config  = read_registers(instrument, UPS_CONFIG, 1, READ_HOLDING_REGISTER)
-    bin_ups_config = bin(ups_config)
-    print("0x%02X - UPS_Config: 0x%02X %s" % (UPS_CONFIG, ups_config, bin_ups_config))
-    
-    # TIME_BUFFERING
-    # default: 600 minutes
-    # Set to 600 minutes
-    write_to_register(instrument, TIME_BUFFERING, 600)
-    # read TIME_BUFFERING
-    time_buffer = read_registers(instrument, TIME_BUFFERING, 1, READ_HOLDING_REGISTER)
-    print("0x%02X - Time Buffer: %d min" % (TIME_BUFFERING, time_buffer))
+        # Meanwell DRS-240-12 or DRS-480-24 DC UPS is connected to the rugged computer using an Advantech Isolated
+        # RS-422/485 to RS-232 Converter BB-4WSD9OTB.
 
-    # Set UPS delay time to 180 seconds
-    write_to_register(instrument, UPS_DELAY_TIME, 180)
-    # Read UPS delay time
-    Delay_time = read_registers(instrument, UPS_DELAY_TIME, 1, READ_HOLDING_REGISTER)
-    print("0x%02X - Delay Time: %d sec" % (UPS_DELAY_TIME, Delay_time))
-    
-    # Read UPS restart time
-    Restart_Time = read_registers(instrument, UPS_RESTART_TIME, 1, READ_HOLDING_REGISTER)
-    print("0x%02X - Restart Time: %d sec" % (UPS_RESTART_TIME, Restart_Time))
+        comPort = sys.argv[1]
+        timer_length = int(sys.argv[2])
 
-    time.sleep(120)
-    file_path = "D:\\modbus\\DRS-240_events.log" #'/Data/modbus/DRS-240_events.log'
-    Status_out = "Startup,system_boot"
-    save_to_file(Status_out, file_path)
+        if comPort != default_port:
+            instrument = minimalmodbus.Instrument(comPort, default_address)
+        events_logger.info('Program Start: Port - %s, Address - 0x%02X',comPort, default_address);
+
+        instrument.serial.baudrate = 115200
+        instrument.serial.bytesize = 8
+        instrument.serial.parity = 'N'
+        instrument.serial.stopbits = 1
+
+        # UPS_CONFIG
+        # default: 0x09
+        ups_config = 0x09
+        # Turn off Wake_Up_EN
+        ups_config = ups_config & 0b00000000
+        # Enable time buffering
+        ups_config = ups_config | 0b00000100
+        # Set UPS_Delay_EN
+        ups_config = ups_config | 0b00010000
+        # Set UPS_Shutdown_EN
+        ups_config = ups_config | 0b00100000
+        # new value: 0x34
+        write_to_register(UPS_CONFIG, ups_config)
+        # read ups_config
+        ups_config  = read_registers(UPS_CONFIG, 1, READ_HOLDING_REGISTER)
+        bin_ups_config = bin(ups_config)
+
+        events_logger.info("0x%02X - UPS_Config: 0x%02X %s" % (UPS_CONFIG, ups_config, bin_ups_config))
+
+        # TIME_BUFFERING
+        # default: 600 minutes
+        # Set T1 time buffer duration
+        if (timer_length < 600) and  (timer_length >= 60):
+            write_to_register(TIME_BUFFERING, timer_length)
+        else:
+            write_to_register(TIME_BUFFERING, default_timer)
+
+        # read TIME_BUFFERING
+        time_buffer = read_registers(TIME_BUFFERING, 1, READ_HOLDING_REGISTER)
+        events_logger.info("0x%02X - Time Buffer: %d min" % (TIME_BUFFERING, time_buffer))
+
+        # Set UPS delay time to 180 seconds
+        write_to_register(UPS_DELAY_TIME, 180)
+        # Read UPS delay time
+        Delay_time = read_registers(UPS_DELAY_TIME, 1, READ_HOLDING_REGISTER)
+        events_logger.info("0x%02X - Delay Time: %d sec" % (UPS_DELAY_TIME, Delay_time))
+
+        # Read UPS restart time
+        Restart_Time = read_registers(UPS_RESTART_TIME, 1, READ_HOLDING_REGISTER)
+        events_logger.info("0x%02X - Restart Time: %d sec" % (UPS_RESTART_TIME, Restart_Time))
+
+        time.sleep(120)
+        file_path = "D:\\modbus\\DRS-240_events.log"
+        Status_out = "Startup,system_boot"
+        events_logger.warning(Status_out)
+
+
+    except KeyboardInterrupt :
+        events_logger.exception("keyboard_interrupt")
+    except Exception as e:
+        events_logger.exception(f"{e}")
+    sys.exit(0)
 
 if __name__ == "__main__":
     main()
